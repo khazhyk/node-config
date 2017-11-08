4968bb4e88ce66d2d4f1e01c7910a2ca63e4e3da
khazhyk <khazhy@gmail.com>
2017-11-07 22:21:01 -0800

    Add hiera
    also other things
    history is gr8

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,23 @@
 node default {
-	include cron_puppet
-	include web
+    include cron_puppet
+    include web
+    class { 'hiera':
+        hiera_version   =>  '5',
+        hiera5_defaults =>  {"datadir" => "data", "data_hash" => "yaml_data"},
+        hierarchy       =>  [
+            {"name" =>  "Virtual yaml", "path"  =>  "virtual/%{::virtual}.yaml"},
+            {"name" =>  "Nodes yaml", "paths" =>  ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
+            {"name" =>  "Default yaml file", "path" =>  "common.yaml"},
+        ],
+        eyaml => true
+
+    }
+    file { 'hmmm':
+        ensure => file,
+        path => "/root/${lookup('key')}",
+        content => "hmmm\n",
+        mode => '0644',
+        owner => root,
+        group => root
+    }
 }
