0fe096d3af50b47c9834e2f181d618927980c455
khazhyk <khazhy@gmail.com>
2017-11-08 21:52:09 -0800

    base pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,17 +1,7 @@
 node default {
+    include base
     include cron_puppet
     include web
-    class { 'hiera':
-        hiera_version   =>  '5',
-        hiera5_defaults =>  {"datadir" => "data", "data_hash" => "yaml_data"},
-        hierarchy       =>  [
-            {"name" =>  "Virtual yaml", "path"  =>  "virtual/%{::virtual}.yaml"},
-            {"name" =>  "Nodes yaml", "paths" =>  ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
-            {"name" =>  "Default yaml file", "path" =>  "common.yaml"},
-        ],
-        eyaml => true
-
-    }
     file { 'hmmm':
         ensure => file,
         path => "/root/${lookup('key')}",
