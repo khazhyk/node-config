1f169f3cda438b4b50275905beedd420b6fc7b39
khazhyk <khazhy@gmail.com>
2017-11-09 21:52:12 -0800

    more hiera

--- modules/base/manifests/sharex.pp
+++ modules/base/manifests/sharex.pp
@@ -1,31 +1,31 @@
 class base::sharex (
-	String $home = '/var/lib/sharex',
-	String $password
+  String $password,
+  String $home = '/var/lib/sharex',
 ) {
-	user { 'sharex_user':
-		name => 'sharex',
-		ensure => present,
-		home => $home,
-		managehome => true,
-		password => $password,
-		system => true,
-	}
-	file { 'sharex_www':
-		path => "${home}/www",
-		ensure => 'directory',
-		owner => 'sharex',
-		group => 'sharex',
-		mode => '0755',
-		require => User['sharex_user'],
-	}
-	nginx::resource::location { 'sharex_nginx':
-		ensure => 'present',
-		www_root => File['sharex_www']['path'],
-		location => '/sx',
-		server => 'khaz.io',
-		require => [
-			File['sharex_www'],
-			Nginx::Resource::Server['khaz.io'],
-		],
-	}
+  user { 'sharex_user':
+    ensure     => present,
+    name       => 'sharex',
+    home       => $home,
+    managehome => true,
+    password   => $password,
+    system     => true,
+  }
+  file { 'sharex_www':
+    ensure  => 'directory',
+    path    => "${home}/www",
+    owner   => 'sharex',
+    group   => 'sharex',
+    mode    => '0755',
+    require => User['sharex_user'],
+  }
+  nginx::resource::location { 'sharex_nginx':
+    ensure   => 'present',
+    www_root => File['sharex_www']['path'],
+    location => '/sx',
+    server   => 'khaz.io',
+    require  => [
+      File['sharex_www'],
+      Nginx::Resource::Server['khaz.io'],
+    ],
+  }
 }
--- modules/base/manifests/web.pp
+++ modules/base/manifests/web.pp
@@ -1,16 +1,16 @@
 class base::web {
-	include nginx
-	file { 'web_www':
-		ensure => 'directory',
-		owner => 'khazhy',
-		group => 'khazhy',
-		mode => '0755',
-		path => "${User['khazhy_user']['home']}/www",
-		require => User['khazhy_user']
-	}
-	nginx::resource::server { 'khaz.io':
-		ensure => present,
-		www_root => "${User['khazhy_user']['home']}/www",
-		require => User['khazhy_user']
-	}
+  include nginx
+  file { 'web_www':
+    ensure  => 'directory',
+    owner   => 'khazhy',
+    group   => 'khazhy',
+    mode    => '0755',
+    path    => "${::User['khazhy_user']['home']}/www",
+    require => User['khazhy_user']
+  }
+  nginx::resource::server { 'khaz.io':
+    ensure   => present,
+    www_root => "${::User['khazhy_user']['home']}/www",
+    require  => User['khazhy_user']
+  }
 }
