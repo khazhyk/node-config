81fad0c47cc4c88de3e2b077686be2498704cb3c
khazhyk <khazhy@gmail.com>
2017-11-09 22:41:26 -0800

    memes

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,12 +3,4 @@ node default {
     include base::web
     include base::sharex
     include cron_puppet
-    file { 'hmmm':
-        ensure  => file,
-        path    => "/root/${lookup('key')}",
-        content => "hmmm\n",
-        mode    => '0644',
-        owner   => root,
-        group   => root
-    }
 }
