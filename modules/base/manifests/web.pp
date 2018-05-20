1dffe24f0574d9978fa9eb4890eb398f378e888e
khazhyk <khazhy@gmail.com>
2018-05-19 18:46:38 -0700

    remove default nginx website

--- modules/base/manifests/web.pp
+++ modules/base/manifests/web.pp
@@ -1,16 +1,17 @@
 class base::web {
-  include nginx
+  class { 'nginx': }
   file { 'web_www':
     ensure  => 'directory',
     owner   => 'khazhy',
     group   => 'khazhy',
     mode    => '0755',
     path    => "${::User['khazhy_user']['home']}/www",
-    require => User['khazhy_user']
+    require => User['khazhy_user'],
   }
   nginx::resource::server { 'khaz.io':
-    ensure   => present,
-    www_root => "${::User['khazhy_user']['home']}/www",
-    require  => User['khazhy_user']
+    ensure               => present,
+    www_root             => "${::User['khazhy_user']['home']}/www",
+    require              => User['khazhy_user'],
+    use_default_location => false,
   }
 }
