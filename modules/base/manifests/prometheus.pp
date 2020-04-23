a7cab3b1c7ae462cf42e47635719c34cffed429a
khazhyk <khazhy@gmail.com>
2020-04-23 08:26:56 +0100

    lint

--- modules/base/manifests/prometheus.pp
+++ modules/base/manifests/prometheus.pp
@@ -1,13 +1,13 @@
 # One prometheus server, and every node has prometheus exporters
 class base::prometheus {
   class { '::prometheus::server':
-    storage_retention        => "90d",
-    scrape_configs           => [
+    storage_retention => '90d',
+    scrape_configs    => [
       {
         'job_name'        => 'prometheus',
         'scrape_interval' => '10s',
         'scrape_timeout'  => '10s',
-        'static_configs' => [{
+        'static_configs'  => [{
           'targets' => ['localhost:9090'],
           'labels'  => {'alias' => 'Prometheus'}
         }]
@@ -15,10 +15,10 @@ class base::prometheus {
       {
         'job_name'        => 'node',
         'scrape_interval' => '5s',
-        'scrape_timeout' => '5s',
+        'scrape_timeout'  => '5s',
         'static_configs'  => [{
-          'targets'         => ['localhost:9100'],
-          'labels'          => {'alias' => 'Node'}
+          'targets' => ['localhost:9100'],
+          'labels'  => {'alias' => 'Node'}
         }]
       }
     ]
